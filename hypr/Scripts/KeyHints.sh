18b96e15f8e6843311521d5a2b46f951a9d89b5c
Sameda1290 <97219311+Sameda1290@users.noreply.github.com>
2024-09-29 17:25:44 +0300

    Add files via upload

---
+++ hypr/Scripts/KeyHints.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Keyhints. Idea got from Garuda Hyprland
+
+# GDK BACKEND. Change to either wayland or x11 if having issues
+BACKEND=wayland
+
+# Check if rofi is running and kill it if it is
+if pgrep -x "rofi" > /dev/null; then
+    pkill rofi
+fi
+
+# Detect monitor resolution and scale
+x_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
+y_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
+hypr_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale' | sed 's/\.//')
+
+# Calculate width and height based on percentages and monitor resolution
+width=$((x_mon * hypr_scale / 100))
+height=$((y_mon * hypr_scale / 100))
+
+# Set maximum width and height
+max_width=1200
+max_height=1000
+
+# Set percentage of screen size for dynamic adjustment
+percentage_width=70
+percentage_height=70
+
+# Calculate dynamic width and height
+dynamic_width=$((width * percentage_width / 100))
+dynamic_height=$((height * percentage_height / 100))
+
+# Limit width and height to maximum values
+dynamic_width=$(($dynamic_width > $max_width ? $max_width : $dynamic_width))
+dynamic_height=$(($dynamic_height > $max_height ? $max_height : $dynamic_height))
+
+# Launch yad with calculated width and height
+GDK_BACKEND=$BACKEND yad --width=$dynamic_width --height=$dynamic_height \
+    --center \
+    --title="Keybindings" \
+    --no-buttons \
+    --list \
+    --column=Key: \
+    --column=Description: \
+    --column=Command: \
+    --timeout-indicator=bottom \
+"ESC" "close this app" "" "=" "SUPER KEY (Windows Key)" "(SUPER KEY)" \
+" enter" "Terminal" "(kitty)" \
+" SHIFT enter" "Dolphin" "(Dolphin)" \
+" SHIFT H" "Help center" "(Keybinds)" \
+" M" "Exit Hyprland" "(SAVE YOUR WORKS!)" \
+" D" "App Launcher" "(rofi)" \
+" Q" "close active window" "" \
+" ALT V" "Clipboard Manager" "(cliphist)" \
+" Z" "Calculator" "(rofi)" \
+" X" "Emoji" "(rofi)" \
+" SHIFT " "Launch Notification Panel" "swaync Notification Center" \
+" Print" "screenshot" "(grim)" \
+" Shift Print" "screenshot region" "(grim + slurp)" \
+" Shift S" "screenshot region" "(swappy)" \
+" CTRL Print" "screenshot timer 5 secs " "(grim)" \
+" CTRL SHIFT Print" "screenshot timer 10 secs " "(grim)" \
